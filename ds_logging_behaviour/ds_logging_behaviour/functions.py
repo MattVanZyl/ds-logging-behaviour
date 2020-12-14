ae0e8ddf3e4643feb5a7ed6c79ac2ee2da6d6186
Matt <m.vanzyl@deakin.edu.au>
2020-12-14 12:39:18 +1100

    NEW: Code now works in using surround framework. Requires a cleanup

--- ds_logging_behaviour/ds_logging_behaviour/functions.py
+++ ds_logging_behaviour/ds_logging_behaviour/functions.py
@@ -1,5 +1,6 @@
 from .fields import LinkFields
 import json
+import csv
 
 def initialise(repo_data):
     api_links = repo_data[LinkFields.API_LINK]
@@ -15,3 +16,145 @@ def read_json(fname):
     with open(fname) as f:
         data = json.load(f)
     return data
+
+
+def export_summary(config, repo_details, log_count, log_levels, log_vs_nonlog):
+    final_list = []
+    cnt = 0
+
+    for repo, repodata in log_levels.items():
+        # To add the all the repo related data in the before hand
+        final_list.append([repo_details[repo]['Type'], repo, repo_details[repo]['Repo Link']])
+        total = {'class_': 0, 'method_': 0, 'end_line_': 0, 'info': 0, 'error': 0, 'warning': 0, 'debug': 0, 'trace': 0,
+                 'fatal': 0}
+        files_count = 0
+        log_level_lines = ""
+        for file_, log_data in repodata.items():
+            files_count += 1
+            for log_name, log_level in log_data.items():
+                if log_name == "end_line_":
+                    total["end_line_"] += log_data['end_line_']["line"]
+                elif log_name in total.keys():
+                    total[log_name] += log_data[log_name]["count"]
+                if log_name in ['info', 'error', 'warning', 'debug', 'trace', 'fatal']:
+                    log_level_lines += log_data['logs'] + ','
+
+        total_log_lines = log_vs_nonlog[repo]['log_lines']
+        if total_log_lines == []:
+            total_log_lines = 'No logs'
+        else:
+            total_log_lines = ""
+            for log_line in log_vs_nonlog[repo]['log_lines']:
+                total_log_lines += log_line
+
+        final_list[cnt].extend(
+            [total['end_line_'], log_count[repo]['logging'], log_count[repo]['trace-traceback'], log_count[repo]['stderr'],
+             log_count[repo]['print'], log_count[repo]['io-file.write'],
+             f"{log_vs_nonlog[repo]['logchanges']} / {log_vs_nonlog[repo]['logchanges'] + log_vs_nonlog[repo]['nonlogchanges']}",
+             total_log_lines, files_count, total['class_'], total['method_'], total['debug'], total['info'],
+             total['warning'], total['error'], total['fatal'], total['trace'], log_level_lines])
+        cnt += 1
+
+    # For adding all the data to the LogMetrics-Summarized.csv
+    with open(f"{config['output_folder']}LogMetrics-Summarized.csv", 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            ['Repo Type', 'Repo Name', 'Repo Link', 'Number of Lines', 'Logging', 'Trace-Traceback', 'Stderr', 'Print',
+             'io-file.write', "DataScience Changes/Total Changes", "Actual Changed Log lines", "Files Count",
+             'Total Classes', 'Total Method', 'Total Debug type', 'Total Info Type', 'Total Warning Type',
+             'Total Error Type', 'Total Fatal type', 'Total Trace type', "Actual Log Lines"])
+        writer.writerows(final_list)
+
+def finalcalc(config, repo_details, log_counts, log_levels, log_vs_nonlog):
+    final_list = []
+    cnt = 0
+
+    for repo_name, level_data in log_levels.items():
+        # To add the all the repo related data in the before hand
+        final_list.append([repo_details[repo_name]['Type'], repo_name, repo_details[repo_name]['Repo Link'], log_counts[repo_name]['logging'],
+                           log_counts[repo_name]['trace-traceback'], log_counts[repo_name]['stderr'], log_counts[repo_name]['print'],
+                           log_counts[repo_name]['io-file.write']])
+        final_list[cnt].extend(["-"] * 23)
+        final_list[cnt].extend([log_vs_nonlog[repo_name]['logchanges'], log_vs_nonlog[repo_name]['nonlogchanges']])
+        cnt += 1
+        # flag_r = 0
+        # Log Level
+        for file_, log_data in level_data.items():
+            class_counts = {}
+            method_counts = {}
+            total_classes, total_methods = 0, 0
+            log_count = 0
+            log_count_sep = {'info': 0, 'error': 0, 'warning': 0, 'debug': 0, 'trace': 0, 'fatal': 0}
+            log_count_sep_class = {'info': 0, 'error': 0, 'warning': 0, 'debug': 0, 'trace': 0, 'fatal': 0}
+            log_count_sep_method = {'info': 0, 'error': 0, 'warning': 0, 'debug': 0, 'trace': 0, 'fatal': 0}
+
+            for log_name, log_level in log_data.items():
+                if log_name in ['info', 'error', 'warning', 'debug', 'trace', 'fatal']:
+                    log_count += log_data[log_name]['count']
+                    log_count_sep[log_name] += log_data[log_name]['count']
+                    for k in log_data[log_name]['lines']:
+                        if 'class_' in log_data.keys():
+                            for c in log_data['class_']['lines']:
+                                if k[0] > c[0] and k[0] < c[1]:
+                                    class_name = 'class_' + str(c[0]) + "-" + str(c[1])
+                                    class_counts[class_name] = class_counts.get(class_name, 0) + 1
+                                    log_count_sep_class[log_name] += 1
+                        if 'method_' in log_data.keys():
+                            for m in log_data['method_']['lines']:
+                                if k[0] > m[0] and k[0] < m[1]:
+                                    method_name = 'method_' + str(m[0]) + "-" + str(m[1])
+                                    method_counts[method_name] = method_counts.get(method_name, 0) + 1
+                                    log_count_sep_method[log_name] += 1
+                else:
+                    if log_name == 'class_':
+                        total_classes += log_data['class_']['count']
+                    elif log_name == 'method_':
+                        total_methods += log_data['method_']['count']
+
+            # Log Density
+            log_den_file, log_den_class, log_den_method = 0, 0, 0
+            numof_lines = log_data['end_line_']['line']
+            if numof_lines != 0:
+                log_den_file = log_count / numof_lines
+            if 'class_' in log_data.keys() and total_classes != 0:
+                log_den_class = log_count / total_classes
+            if 'method_' in log_data.keys() and total_methods != 0:
+                log_den_method = log_count / total_methods
+            # if flag_r == 1:
+            final_list.append([""] * 8)
+            final_list[cnt].extend(
+                [file_, log_data['end_line_']['line'], log_den_file, log_den_class, log_den_method,
+                 log_count_sep['info'], log_count_sep['error'], log_count_sep['warning'],
+                 log_count_sep['debug'], log_count_sep['trace'], log_count_sep['fatal'],
+                 log_count_sep_class['info'], log_count_sep_class['error'], log_count_sep_class['warning'],
+                 log_count_sep_class['debug'], log_count_sep_class['trace'], log_count_sep_class['fatal'],
+                 log_count_sep_method['info'], log_count_sep_method['error'], log_count_sep_method['warning'],
+                 log_count_sep_method['debug'], log_count_sep_method['trace'], log_count_sep_method['fatal'],
+                 "", ""])
+            '''if flag_r == 0:
+                final_list[cnt].extend(["-"] * 23)
+                final_list[cnt].extend([logvsnlog[repo]['logchanges'] , logvsnlog[repo]['nonlogchanges']])'''
+
+            # flag_r = 1
+            cnt += 1
+
+        '''if flag_r == 0:
+            final_list[cnt].extend(["-"] * 23)'''
+
+
+    # For adding all the data to the FINAL.csv
+    with open(f"{config['output_folder']}FINAL.csv", 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['DataScience/NonDataScience', 'Repo Name', 'Repo Link', 'Instances of Log - Logger',
+                         'Instances of Log - Trace/Traceback', 'Instances of Log -StdErr',
+                         'Instances of Log - Print', 'Instances of Log - IO/File.write', 'FileName',
+                         'Log Density - Lines', 'Log Density - File', 'Log Density - Class',
+                         'Log Density - Method', 'Log Level File - Info', 'Log Level File - Error',
+                         'Log Level File - Warning', 'Log Level File - Debug', 'Log Level File - Trace',
+                         'Log Level File - Fatal', 'Log Level Class - Info', 'Log Level Class - Error',
+                         'Log Level Class - Warning', 'Log Level Class - Debug', 'Log Level Class - Trace',
+                         'Log Level Class - Fatal', 'Log Level Method - Info', 'Log Level Method - Error',
+                         'Log Level Method - Warning', 'Log Level Method - Debug', 'Log Level Method - Trace',
+                         'Log Level Method - Fatal', 'DataScience Related Changes',
+                         'Non DataScience Related Changes'])
+        writer.writerows(final_list)
--- ds_logging_behaviour/ds_logging_behaviour/stages/data_extractor.py
+++ ds_logging_behaviour/ds_logging_behaviour/stages/data_extractor.py
@@ -10,49 +10,139 @@ import os
 from pathlib import Path
 import glob
 
-from ..functions import initialise, save_json, read_json
+import subprocess
+import json
+import re
+
+from ..functions import initialise, finalcalc, export_summary
 
 LOGGER = logging.getLogger(__name__)
+
 
 class DataExtractor(Stage):
     def operate(self, state, config):
 
         api_links, repo_links, types = initialise(state.input_data)
-        Path(config['input_folder'] + "log_instances/").mkdir(parents=True, exist_ok=True)
-
         working_dir = Path().absolute()
-        print(working_dir)
 
+        repo_details = {}
+        repo_log_counts = {}
+        repo_log_levels = {}
+
+        # For each repo...
         for i in range(api_links.count()):
-            repo_name = repo_links[i].split('/')[-1]
-            print(repo_name + " -- " + api_links[i])
-
-            # Create repo directories
-            dir = config['input_folder'] + "repo_data/" + types[i]
-            Path(dir).mkdir(parents=True, exist_ok=True)
-
-            os.chdir(f"{dir}/")
-
-            # os.system(f"git clone {repo_links[i]}")
-            # os.system(f"semgrep -f {config['semgrep_logcount']} {repo_name} --json -o {working_dir}/{config['input_folder']}log_instances/{repo_name}.json")
-
+            # Reset to the working directory
             os.chdir(f"{working_dir}")
 
-        self.semgrep2json(config)
-        os.chdir(f"{working_dir}")
+            # Get the name of the repo
+            repo_name = repo_links[i].split('/')[-1]
 
-    def semgrep2json(self, config):
-        os.chdir(f"{config['input_folder']}log_instances/")
-        path = '*.json'
-        repo_log_count = {}
-        for reponame in glob.glob(path, recursive = True):
-            repo_log_count[reponame.split('.')[0]] = {'print' : 0, 'logging' : 0, 'trace-traceback' : 0, 'io-file.write' : 0,  'stderr' : 0}
-            file_logcount = read_json(reponame)
-            for file_in_repo in file_logcount['results']:
-                #for stoping the redundancy of sys.stderr in io and stdrr, """ is to avoid conflict when formatting is used
+            repo_details[repo_name] = {}
+            repo_details[repo_name]['Type'] = types[i]
+            repo_details[repo_name]['Repo Link'] = repo_links[i]
+
+            # Create a data_science and non_data_science directory
+            dir = f"{config['input_folder']}repo_data/{types[i]}"
+            Path(dir).mkdir(parents=True, exist_ok=True)
+
+            # Clone the repo
+            os.chdir(f"{dir}/")
+            os.system(f"git clone {repo_links[i]}")
+
+            # =============================================
+            # Log Count
+            # =============================================
+            repo_log_count = {}
+            output = json.loads(
+                subprocess.check_output(f"semgrep -f {config['semgrep_logcount']} {repo_name} --json", shell=True))
+
+            repo_log_counts[repo_name] = {'print': 0, 'logging': 0, 'trace-traceback': 0, 'io-file.write': 0,
+                                         'stderr': 0}
+
+            for file_in_repo in output['results']:
+                # for stoping the redundancy of sys.stderr in io and stdrr, """ is to avoid conflict when formatting is used
                 if file_in_repo['check_id'] == 'stderr':
-                    repo_log_count[reponame.split('.')[0]]['io-file.write'] -= 1
-                repo_log_count[reponame.split('.')[0]][file_in_repo['check_id']] += 1
-        save_json(repo_log_count, f"{config['input_folder']}log_instances/log_count.json")
+                    repo_log_counts[repo_name]['io-file.write'] -= 1
+                repo_log_counts[repo_name][file_in_repo['check_id']] += 1
+
+            # =============================================
+            # Log Level
+            # =============================================
+            output2 = json.loads(subprocess.check_output(f"semgrep -f {config['semgrep_loglevel']} {repo_name} --json", shell=True))
 
 
+
+            repo_log_levels[repo_name] = {}
+            info_log = {}
+            for i in range(len(output2['results'])):
+                f_name_list = output2['results'][i]['path']
+                f_name = f_name_list.split('/')[-1]
+                repo_log_levels[repo_name].setdefault(f_name, {})
+                log_type = output2['results'][i]['check_id']
+                repo_log_levels[repo_name][f_name].setdefault('end_line_', {'line': 0})
+                repo_log_levels[repo_name][f_name].setdefault('logs', "")
+                t_line = ""
+                if log_type in ['info', 'error', 'warning', 'debug', 'trace', 'fatal']:
+                    t_line = output2['results'][i]['extra']['lines']
+                    repo_log_levels[repo_name][f_name]["logs"] += t_line + ','
+                if log_type == 'end_line_':
+                    repo_log_levels[repo_name][f_name][log_type]['line'] = output2['results'][i]['end']['line'] - 1
+                else:
+                    repo_log_levels[repo_name][f_name].setdefault(log_type, {'lines': [], 'count': 0})
+                    repo_log_levels[repo_name][f_name][log_type]['count'] += 1
+                    # To remove the redundancy of count in info when it matches with other log
+                    info_log.setdefault(f_name, [])
+                    if log_type == 'info':
+                        info_log[f_name].append(
+                            [output2['results'][i]['extra']['lines'], output2['results'][i]['start']['col']])
+                    # to elimate the matching confusion for a statement with two logs
+                    elif [output2['results'][i]['extra']['lines'], output2['results'][i]['start']['col']] in info_log[
+                        f_name]:
+                        repo_log_levels[repo_name][f_name]['info']['count'] -= 1
+                    start, end = output2['results'][i]['start']['line'], output2['results'][i]['end']['line']
+                    repo_log_levels[repo_name][f_name][log_type]['lines'].append([start, end])
+
+            # =============================================
+            # Log Vs. Non-Log
+            # =============================================
+            os.chdir(f"{working_dir}")
+
+            # Create a data_science and non_data_science directory
+            log_vs_nlog = f"{config['input_folder']}log_vs_nonlog/"
+            Path(log_vs_nlog).mkdir(parents=True, exist_ok=True)
+
+            repo_path = f"{dir}/{repo_name}/"
+            os.system(
+                f"git --git-dir={repo_path}.git log -10 -p '*.py' | grep '^[+-]' | grep -Ev '^(--- a/|\+\+\+ b/)'> {log_vs_nlog}{repo_name}.txt")
+
+            # =============================================
+
+
+        log_vs_nonlog = changesjson(config)
+        finalcalc(config, repo_details, repo_log_counts, repo_log_levels, log_vs_nonlog)
+        # export_summary(config, repo_details, repo_log_counts, repo_log_levels, log_vs_nonlog)
+
+# changes json for adding the data to the final format
+def changesjson(config):
+    files = f"{config['input_folder']}log_vs_nonlog/*.txt"
+    logvsnlog_changes = {}
+    for repo_changes in glob.glob(files, recursive=True):
+        log_lines = []
+        total_lines, logc = 0, 0
+        name = repo_changes.split('/')[-1]
+        with open(repo_changes) as rc:
+            for line in rc:
+                total_lines += 1
+                temp_line = line[1:].strip()
+                if len(temp_line) != 0:
+                    regexList = ['^print', 'io\.', '^trace\.', '^traceback\.', 'logging\.', '^sys\.stderr\.write',
+                                 '.*\.write']
+                    for regex in regexList:
+                        s = re.search(regex, temp_line)
+                        if s:
+                            log_lines.append(temp_line)
+                            logc += 1
+                            break
+        logvsnlog_changes[name.split('.')[0]] = {'logchanges': logc, 'nonlogchanges': total_lines - logc,
+                                                 'log_lines': log_lines}
+    return logvsnlog_changes
