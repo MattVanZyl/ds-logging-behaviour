5df242640e94fe7b3ca4fa74b6495c193cb3adaf
Matt <m.vanzyl@deakin.edu.au>
2020-12-18 16:13:49 +1100

    NEW: Added a git ignore for the repo folder. Now also save a csv of all failed repo downloads

--- ds_logging_behaviour/ds_logging_behaviour/stages/repo_downloader.py
+++ ds_logging_behaviour/ds_logging_behaviour/stages/repo_downloader.py
@@ -5,6 +5,8 @@ from pydriller import GitRepository, RepositoryMining
 from git.exc import GitCommandError, NoSuchPathError
 import logging
 from pathlib import Path
+import pandas as pd
+
 
 class RepoDownloader(Stage):
     def operate(self, state, config):
@@ -14,6 +16,8 @@ class RepoDownloader(Stage):
             f"\n{PrintColours.CYAN}{PrintColours.BOLD}-----------------------\nSetting Up Repositories\n-----------------------{PrintColours.RESET}")
 
         repo_count = 1
+
+        failed_downloads_df = pd.DataFrame(columns=['number','name','url'])
 
         for repo_name in repositories.keys():
             download_folder = f"{config['repositories_path']}repository_{repo_count}"
@@ -26,7 +30,8 @@ class RepoDownloader(Stage):
             try:
                 # Check if repo already exists
                 GitRepository(local_path)._open_repository()
-                logging.info(f" {repo_count}. {PrintColours.BLUE}{repo_name}{PrintColours.RESET} - {PrintColours.YELLOW}Already downloaded{PrintColours.RESET}")
+                logging.info(
+                    f" {repo_count}. {PrintColours.BLUE}{repo_name}{PrintColours.RESET} - {PrintColours.YELLOW}Already downloaded{PrintColours.RESET}")
 
             except NoSuchPathError:
                 try:
@@ -35,13 +40,18 @@ class RepoDownloader(Stage):
                         f" {repo_count}. {PrintColours.BLUE}{repo_name}{PrintColours.RESET} - {PrintColours.YELLOW}Cloning...{PrintColours.RESET}")
                     Path(download_folder).mkdir(parents=True, exist_ok=True)
                     # Clone the specified commit, if no commit is provided then clone the latest
-                    RepositoryMining(repositories[repo_name]['url'],from_commit=repositories[repo_name]['commit'])._clone_remote_repo(tmp_folder=download_folder,repo=repositories[repo_name]['url'])
+                    RepositoryMining(repositories[repo_name]['url'],
+                                     from_commit=repositories[repo_name]['commit'])._clone_remote_repo(
+                        tmp_folder=download_folder, repo=repositories[repo_name]['url'])
 
                 except GitCommandError as err:
                     logging.info(
                         f" {repo_count}. {PrintColours.BLUE}{repo_name}{PrintColours.RESET} - {PrintColours.RED}Download failed:{PrintColours.RESET}\n{err.stderr}")
+                    failed_downloads_df = failed_downloads_df.append({'number': repo_count,'name': repo_name,'url': repositories[repo_name]['url']}, ignore_index=True)
 
             repo_count += 1
+
+        failed_downloads_df.to_csv(f"{config['repositories_path']}failed_downloads.csv", index=False)
 
     def initialise(self, repo_data):
         repo_details = {}
