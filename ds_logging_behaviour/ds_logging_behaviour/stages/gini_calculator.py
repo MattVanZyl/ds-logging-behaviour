0f7dd2ab0123bc0c991fa8695c55778159ad5a87
Matt <m.vanzyl@deakin.edu.au>
2021-03-02 17:52:07 +1100

    NEW: Added support for gini indexes of module, function, class and method

--- ds_logging_behaviour/ds_logging_behaviour/stages/gini_calculator.py
+++ ds_logging_behaviour/ds_logging_behaviour/stages/gini_calculator.py
@@ -2,42 +2,88 @@ from surround import Stage
 from ..color import Color
 import logging
 import pandas as pd
+from enum import Enum
 
+
+class ScopeType(Enum):
+    FILE = "file"
+    MODULE = "module"
+    FUNCTION = "function"
+    CLASS = "class"
+    METHOD = "method"
 
 class GiniCalculator(Stage):
-    def operate(self, state, config):
-        logging.info(
-            f"\n{Color.CYAN}{Color.BOLD}---------------------------------\nCalculating Gini Indexes\n---------------------------------{Color.RESET}")
+    def gini(self, list_of_values):
+        sorted_list = sorted(list_of_values)
+        height, area = 0, 0
+        for value in sorted_list:
+            height += value
+            area += height - value / 2.
+        fair_area = height * len(list_of_values) / 2.
+        return (fair_area - area) / fair_area
 
-        logs_df = pd.read_csv(f"{config['path_output']}{config['output_logs']}")
-        metrics_df = pd.read_csv(f"{config['path_output']}{config['output_metrics']}")
+    # def calculateRepoGini(self, logs_df, repository_id, count_module):
+    #     gini_index_repo = 0
+    #
+    #     repo_logs = logs_df.loc[logs_df["repository-id"] == repository_id]
+    #     # Count the number of logs in each file and put the counts in an array
+    #     logs_counts = []
+    #
+    #     did_find_logs_for_repo = not repo_logs.empty
+    #
+    #     if did_find_logs_for_repo:
+    #
+    #         for count in repo_logs["relative-file-path"].value_counts():
+    #             logs_counts.append(count)
+    #
+    #         # Create an array of log counts per file in the repo and initialise each to 0
+    #         total_logs_per_file = [0] * count_module
+    #
+    #         # logging.info(f"Logs per file BEFORE: {total_logs_per_file}")
+    #         # logging.info(f"logs_counts: {logs_counts}")
+    #
+    #         # Replace elements at the start with the counts saved earlier
+    #         if len(logs_counts) > 0:
+    #             total_logs_per_file[0:len(logs_counts)] = logs_counts
+    #
+    #             # logging.info(f"Logs per file AFTER: {total_logs_per_file}")
+    #
+    #             gini_index_repo = self.gini(total_logs_per_file)
+    #
+    #     return gini_index_repo
 
-        gini_df = pd.DataFrame(
-            columns=['repository-id', 'repository-name', 'repository-type', 'gini-index-repo'])
+    def calculateScopeGini(self, logs_df, repository_id, scope: ScopeType, count_scope, ):
 
-        for index, row in metrics_df.iterrows():
-            repository_id = row["repository-id"]
-            repository_name = row["repository-name"]
-            repository_type = row["repository-type"]
-            count_module = row["module-count"]
+        def get_scope_columns():
+            logging.info(f"{scope} == {ScopeType.FILE} : {scope == ScopeType.FILE}")
+            if scope == ScopeType.FILE:
+                return logs_df.loc[logs_df["repository-id"] == repository_id]
+            else:
+                return logs_df.loc[(logs_df["repository-id"] == repository_id) & (logs_df["log-scope"] == scope.value)]
 
-            # Get all the logs of the current repo
-            repo_logs = logs_df.loc[logs_df['repository-id'] == repository_id]
+        def get_scope_log_counts():
+            if scope == ScopeType.FILE:
+                return repo_logs["relative-file-path"].value_counts()
+            else:
+                return repo_logs.groupby(["relative-file-path", "log-scope-id"]).size()
 
-            logging.info(f"REPO: {repository_id} - {repository_name}")
+        logging.info(f" - {scope.name} - ")
+        logging.info(f"{scope.name} Count: {count_scope}")
 
-            # Gini Repo - Logs per file
-            # ---------------------
 
-            # Count the number of logs in each file and put the counts in an array
-            logs_counts = []
-            for count in repo_logs["relative-file-path"].value_counts():
+        gini_index_scope = 0
+
+        # repo_logs = logs_df.loc[(logs_df["repository-id"] == repository_id) & (logs_df["log-scope"] == scope)]
+        repo_logs = get_scope_columns()
+
+        logs_counts = []
+
+        if not repo_logs.empty:
+            for count in get_scope_log_counts(): #repo_logs.groupby(["relative-file-path", "log-scope-id"]).size():
                 logs_counts.append(count)
 
             # Create an array of log counts per file in the repo and initialise each to 0
-            total_logs_per_file = [0] * count_module
-
-            gini_index_file = 0
+            total_logs_per_file = [0] * count_scope
 
             logging.info(f"Logs per file BEFORE: {total_logs_per_file}")
             logging.info(f"logs_counts: {logs_counts}")
@@ -48,23 +94,91 @@ class GiniCalculator(Stage):
 
                 logging.info(f"Logs per file AFTER: {total_logs_per_file}")
 
-                gini_index_file = self.gini(total_logs_per_file)
+                gini_index_scope = self.gini(total_logs_per_file)
 
-                logging.info(f"Gini index: {gini_index_file}")
+        return gini_index_scope
+
+    def operate(self, state, config):
+        logging.info(
+            f"\n{Color.CYAN}{Color.BOLD}---------------------------------\nCalculating Gini Indexes\n---------------------------------{Color.RESET}")
+
+        logs_df = pd.read_csv(f"{config['path_output']}{config['output_logs']}")
+        metrics_df = pd.read_csv(f"{config['path_output']}{config['output_metrics']}")
+
+        gini_df = pd.DataFrame(
+            columns=['repository-id',
+                     'repository-name',
+                     'repository-type',
+                     'gini-index-file',
+                     'gini-index-module',
+                     'gini-index-function',
+                     'gini-index-class',
+                     'gini-index-method'])
+
+        for index, row in metrics_df.iterrows():
+            repository_id = row["repository-id"]
+            repository_name = row["repository-name"]
+            repository_type = row["repository-type"]
+            count_module = row["module-count"]
+            count_function = row["function-count"]
+            count_class = row["class-count"]
+            count_method = row["method-count"]
+
+
+            # Get all the logs of the current repo
+            # repo_logs = logs_df.loc[logs_df['repository-id'] == repository_id]
+
+            logging.info(f"\n\nREPO: {repository_id} - {repository_name}")
+
+            gini_index_file = self.calculateScopeGini(logs_df, repository_id, ScopeType.FILE, count_module)
+            logging.info(f"gini_index_file: {gini_index_file}")
+            gini_index_module = self.calculateScopeGini(logs_df, repository_id, ScopeType.MODULE, count_module)
+            logging.info(f"gini_index_module: {gini_index_module}")
+            gini_index_function = self.calculateScopeGini(logs_df, repository_id, ScopeType.FUNCTION, count_function)
+            logging.info(f"gini_index_function: {gini_index_function}")
+            gini_index_class = self.calculateScopeGini(logs_df, repository_id, ScopeType.CLASS, count_class)
+            logging.info(f"gini_index_class: {gini_index_class}")
+            gini_index_method = self.calculateScopeGini(logs_df, repository_id, ScopeType.METHOD, count_method)
+            logging.info(f"gini_index_method: {gini_index_method}")
+
+            # # Gini Repo - Logs per file
+            # # ---------------------
+            #
+            # # Count the number of logs in each file and put the counts in an array
+            # logs_counts = []
+            # for count in repo_logs["relative-file-path"].value_counts():
+            #     logs_counts.append(count)
+            #
+            # # Create an array of log counts per file in the repo and initialise each to 0
+            # total_logs_per_file = [0] * count_module
+            #
+            # gini_index_file = 0
+            #
+            # logging.info(f"Logs per file BEFORE: {total_logs_per_file}")
+            # logging.info(f"logs_counts: {logs_counts}")
+            #
+            # # Replace elements at the start with the counts saved earlier
+            # if len(logs_counts) > 0:
+            #     total_logs_per_file[0:len(logs_counts)] = logs_counts
+            #
+            #     logging.info(f"Logs per file AFTER: {total_logs_per_file}")
+            #
+            #     gini_index_file = self.gini(total_logs_per_file)
+            #
+            #     logging.info(f"Gini index: {gini_index_file}")
 
             # Save Gini Indexes
             gini_df = gini_df.append(
-                {'repository-id': repository_id, 'repository-name': repository_name, 'repository-type': repository_type,
-                 'gini-index-repo': gini_index_file},
+                {'repository-id': repository_id,
+                 'repository-name': repository_name,
+                 'repository-type': repository_type,
+                 'gini-index-file': gini_index_file,
+                 'gini-index-module': gini_index_module,
+                 'gini-index-function': gini_index_function,
+                 'gini-index-class': gini_index_class,
+                 'gini-index-method': gini_index_method},
                 ignore_index=True)
 
             gini_df.to_csv(f"{config['path_output']}{config['output_gini_indexes']}", index=False)
 
-    def gini(self, list_of_values):
-        sorted_list = sorted(list_of_values)
-        height, area = 0, 0
-        for value in sorted_list:
-            height += value
-            area += height - value / 2.
-        fair_area = height * len(list_of_values) / 2.
-        return (fair_area - area) / fair_area
+
--- ds_logging_behaviour/ds_logging_behaviour/stages/visualization.py
+++ ds_logging_behaviour/ds_logging_behaviour/stages/visualization.py
@@ -10,8 +10,22 @@ class Visualization(Stage):
             f"\n{Color.CYAN}{Color.BOLD}---------------------------------\nVisualization\n---------------------------------{Color.RESET}")
 
         df = pd.read_csv(f"{config['path_output']}{config['output_gini_indexes']}")
-        plot_density = sns.displot(data=df, x="gini-index-repo", hue="repository-type", kind="kde", fill=True, cut=0)
-        plot_density.savefig(f"{config['path_output']}plot_density.png")
+        #FILE
+        plot_density = sns.displot(data=df, x="gini-index-file", hue="repository-type", kind="kde", fill=True, cut=0)
+        plot_density.savefig(f"{config['path_output']}plot_density_file.png")
 
-        plot_histo = sns.displot(data=df, x="gini-index-repo", hue="repository-type", multiple="dodge")
-        plot_histo.savefig(f"{config['path_output']}plot_histogram.png")
+        plot_histo = sns.displot(data=df, x="gini-index-file", hue="repository-type", multiple="dodge").set_title('Logs per file')
+        plot_histo.savefig(f"{config['path_output']}plot_histogram_file.png")
+
+        plot_histo = sns.displot(data=df, x="gini-index-module", hue="repository-type", multiple="dodge").set_title('Logs per module (outside of classes, functions and methods)')
+        plot_histo.savefig(f"{config['path_output']}plot_histogram_module.png")
+
+        plot_histo = sns.displot(data=df, x="gini-index-function", hue="repository-type", multiple="dodge").set_title('Logs per function')
+        plot_histo.savefig(f"{config['path_output']}plot_histogram_function.png")
+
+        plot_histo = sns.displot(data=df, x="gini-index-class", hue="repository-type", multiple="dodge").set_title('Logs per class (outside of methods)')
+        plot_histo.savefig(f"{config['path_output']}plot_histogram_class.png")
+
+        plot_histo = sns.displot(data=df, x="gini-index-method", hue="repository-type", multiple="dodge").set_title('Logs per method')
+        plot_histo.savefig(f"{config['path_output']}plot_histogram_method.png")
+
